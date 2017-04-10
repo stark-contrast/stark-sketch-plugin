ec099a7c99435e6d08caf8583031212bf47cbed4
Michael Fouquet <michael.fouquet@hudl.com>
2017-04-09 22:46:53 -0500

    Get it set up to simulate artboards

--- stark/a11yPlugin.sketchplugin/Contents/Resources/Web/main.js
+++ stark/a11yPlugin.sketchplugin/Contents/Resources/Web/main.js
@@ -1,9 +1,27 @@
 var simSelect = document.getElementById('SimulationSelect');
 
+var artboardSelect = document.getElementById('ArtboardSelect');
+
+var opt1 = document.createElement('option');
+opt1.value = "";
+opt1.innerHTML = "";
+artboardSelect.appendChild(opt1);
+
+for (var i = 0; i < artboardNames.length; i++){
+    var opt = document.createElement('option');
+    opt.value = "abid_" + artboardNames[i];
+    opt.innerHTML = artboardNames[i];
+    artboardSelect.appendChild(opt);
+}
+
 simSelect.addEventListener("change", function(event) {
   window.status = simSelect.value;
   var simId = simSelect.value.replace('id_', '');
   runSimulation(simId);
+});
+
+artboardSelect.addEventListener("change", function(event) {
+  window.status = artboardSelect.value;
 });
 
 document.addEventListener("DOMContentLoaded", function(event) {
